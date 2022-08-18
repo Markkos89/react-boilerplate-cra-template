4b5e5e018ba8319e11360dcfc42c0a7d92b04fca
Caleb Hoff <crunchycat1@gmail.com>
2022-08-18 02:49:02 -0500

    final formatting

--- internals/startingTemplate/src/app/index.tsx
+++ internals/startingTemplate/src/app/index.tsx
@@ -4,7 +4,7 @@
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
-*/
+ */
 
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
@@ -15,7 +15,7 @@ import { GlobalStyle } from '../styles/global-styles';
 import { HomePage } from './pages/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import { useTranslation } from 'react-i18next';
- 
+
 export function App() {
   const { i18n } = useTranslation();
   return (
